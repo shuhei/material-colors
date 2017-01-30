204d7e9210ded88054766c9737b3c0738038c2e5
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2017-01-30 23:23:10 +0100

    Use rollup API and tmp file to avoid using pipe

--- test/module.js
+++ test/module.js
@@ -1,28 +1,29 @@
+var test = require('tape');
 var fs = require('fs');
 var path = require('path');
 var os = require('os');
+var tmp = require('tmp');
+var rollup = require('rollup');
 var execSync = require('child_process').execSync;
 
 var node = process.argv[0];
 var fixtures = fs.readdirSync(path.join(__dirname, 'fixtures'));
-var failed = false;
+
 fixtures.forEach(function (fixture) {
-  var filename = path.join(__dirname, 'fixtures', fixture);
-  var command = [
-    path.join(__dirname, '..', 'node_modules', '.bin', 'rollup'),
-    '-f es',
-    filename,
-    '|',
-    node
-  ].join(' ');
-  var stdout = execSync(command).toString();
-  if (stdout === '#ffebee' + os.EOL) {
-    console.error('OK', fixture);
-  } else {
-    failed = true;
-    console.error('NG', fixture, ':', stdout);
-  }
+  test('module: ' + fixture, function (t) {
+    t.plan(1);
+
+    var filename = path.join(__dirname, 'fixtures', fixture);
+    rollup.rollup({ entry: filename })
+      .then(function (bundle) {
+        var js = bundle.generate({ format: 'es' }).code;
+
+        var tmpfile = tmp.fileSync().name;
+        var command = [node, tmpfile].join(' ');
+        fs.writeFileSync(tmpfile, js);
+        var stdout = execSync(command).toString();
+
+        t.equal(stdout, '#ffebee' + os.EOL);
+      });
+  });
 });
-if (failed) {
-  process.exit(1);
-}
